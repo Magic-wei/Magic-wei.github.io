d1af119f98d89c93bc17670bfd7d611e54180227
magic-wei <wei_company@126.com>
2019-02-15 13:24:48 +0800

    init personal website

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Videos about My Work",
         "excerpt":"Dozens of videos about my motion planning and simulations work to come! Hybrid Motion Planning Library (HMPL) HMPL Highlights (state of HMPL in 2017) Semi-global Path Modification Layer Derivative-free Path Optimization Algorithm Multiple-stage State Space Sampling Path Planning Algorithm State Space Sampling Local Planner Simulation Fast Scene Modelling Experiments Forward-simulation...","categories": [],
         "tags": [],
-        "url": "https://magic-wei.github.io/videos/",
-        "teaser":"https://magic-wei.github.io/assets/images/default_teaser.jpg"},]
+        "url": "http://localhost:4000/videos/",
+        "teaser":"http://localhost:4000/assets/images/default_teaser.jpg"},]
